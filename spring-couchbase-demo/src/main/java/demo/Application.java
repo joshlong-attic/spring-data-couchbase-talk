60fc1b3038e5bf2ee04cd4146136e4ed8fe73cfd
Josh Long <josh@joshlong.com or josh.long@springsource.com>
2014-10-05 19:13:40 -0700

    cleanup

--- spring-couchbase-demo/src/main/java/demo/Application.java
+++ spring-couchbase-demo/src/main/java/demo/Application.java
@@ -1,14 +1,11 @@
 package demo;
 
-import com.couchbase.client.protocol.views.Query;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.couchbase.config.AbstractCouchbaseConfiguration;
-import org.springframework.data.repository.CrudRepository;
 
 import java.util.Arrays;
 import java.util.List;
@@ -47,38 +44,4 @@ public class Application extends AbstractCouchbaseConfiguration {
     }
 }
 
-interface ActivityRepository extends CrudRepository<Activity, String> {
-    List<Activity> findByOrigin(Query query);
-}
 
-class Activity {
-
-    @Id
-    private String id;
-    private String content;
-    private String origin;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getOrigin() {
-        return origin;
-    }
-
-    public void setOrigin(String origin) {
-        this.origin = origin;
-    }
-}
