86ca9a885efb91423ca139b76fa18c7ccb139b77
Josh Long <josh@joshlong.com or josh.long@springsource.com>
2014-10-05 19:17:24 -0700

    commit

--- spring-couchbase-demo/src/main/java/demo/Application.java
+++ spring-couchbase-demo/src/main/java/demo/Application.java
@@ -1,11 +1,14 @@
 package demo;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.couchbase.config.AbstractCouchbaseConfiguration;
+import org.springframework.data.couchbase.core.CouchbaseTemplate;
 
 import java.util.Arrays;
 import java.util.List;
@@ -41,6 +44,20 @@ public class Application extends AbstractCouchbaseConfiguration {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(CouchbaseTemplate couchbaseTemplate) {
+        return args -> {
+
+            // work with the couchbase template
+
+            // use the Couchbase cache manager
+
+            // use the Spring Session integration
+
+            // 
+        };
     }
 }
 
