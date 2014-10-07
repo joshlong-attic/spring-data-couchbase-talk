cf6221dc1416938bb4bba6f1304a8302a97f3f07
Josh Long <josh@joshlong.com or josh.long@springsource.com>
2014-10-06 17:31:46 -0700

    polish

--- spring-couchbase-demo/src/main/java/demo/Application.java
+++ spring-couchbase-demo/src/main/java/demo/Application.java
@@ -2,11 +2,16 @@ package demo;
 
 import com.couchbase.client.CouchbaseClient;
 import com.couchbase.client.protocol.views.Query;
+import com.couchbase.client.protocol.views.Stale;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -19,22 +24,25 @@ import org.springframework.data.couchbase.core.mapping.Field;
 import org.springframework.data.couchbase.core.mapping.event.ValidatingCouchbaseEventListener;
 import org.springframework.data.couchbase.repository.config.EnableCouchbaseRepositories;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.social.facebook.api.Facebook;
 import org.springframework.social.facebook.api.Page;
 import org.springframework.social.facebook.api.impl.FacebookTemplate;
+import org.springframework.stereotype.Service;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 import java.io.IOException;
 import java.io.StringWriter;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
 @EnableCouchbaseRepositories
+@EnableScheduling
+@EnableCaching
 public class Application extends AbstractCouchbaseConfiguration {
 
     @Value("${couchbase.cluster.bucket}")
@@ -45,6 +53,9 @@ public class Application extends AbstractCouchbaseConfiguration {
 
     @Value("${couchbase.cluster.ip}")
     private String ip;
+
+    @Value("${facebook.accessToken}")
+    private String accessToken;
 
     @Override
     protected List<String> bootstrapHosts() {
@@ -63,13 +74,14 @@ public class Application extends AbstractCouchbaseConfiguration {
 
     @Bean
     Facebook facebook() {
-        return new FacebookTemplate("594228000699830|LNmCmAnJrszIbU835yOfP0Yk6IU");
+        return new FacebookTemplate(
+                accessToken);
     }
 
     @Bean
-    CouchbaseCacheManager cacheManager() throws Exception {
+    CouchbaseCacheManager cacheManager(CouchbaseClient couchbaseClient) throws Exception {
         HashMap<String, CouchbaseClient> instances = new HashMap<>();
-        instances.put("persistent", couchbaseClient());
+        instances.put("places", couchbaseClient);
         return new CouchbaseCacheManager(instances);
     }
 
@@ -83,39 +95,21 @@ public class Application extends AbstractCouchbaseConfiguration {
         return new ValidatingCouchbaseEventListener(validator());
     }
 
-    private String json(Object o) {
-        StringWriter stringWriter = new StringWriter();
-        ObjectMapper mapper = new ObjectMapper();
-        try {
-            mapper.writeValue(stringWriter, o);
-            return stringWriter.toString();
-        }
-        catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
 
     @Bean
     CommandLineRunner commandLineRunner(
             CouchbaseClient couchbaseClient,
             PlaceRepository placeRepository,
             CouchbaseTemplate couchbaseTemplate,
-            Facebook facebook) {
+            PlaceService placeService) {
         return args -> {
-
             couchbaseClient.flush().get();
 
-            facebook.placesOperations().search("Starbucks", 37.752494, -122.414166, 5280).forEach((Page p) -> {
-                couchbaseTemplate.insert(new Place(p));
-             });
-            Query q = new Query();
+            placeService.search("Starbucks", 37.752494, -122.414166, 5280)
+                    .forEach(id -> {
+                        System.out.println(json(placeRepository.findOne(id)));
+                    });
 
-            // low level query call
-//            couchbaseTemplate.findByView("place","all",q,Place.class);
-//            placeRepository.findAll().forEach(System.out::println);
-            // work with the couchbase template
-
-            // use the Couchbase cache manager
 
             // use the Spring Session integration
 
@@ -126,11 +120,70 @@ public class Application extends AbstractCouchbaseConfiguration {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+    private String json(Object o) {
+        StringWriter stringWriter = new StringWriter();
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            mapper.writeValue(stringWriter, o);
+            return stringWriter.toString();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+@Service
+class PlaceService {
+
+    private final CacheManager cacheManager;
+
+    private final Facebook facebook;
+
+    private final CouchbaseTemplate couchbaseTemplate;
+
+    private final PlaceRepository placeRepository;
+
+    @Autowired
+    PlaceService(CacheManager cacheManager, Facebook facebook, CouchbaseTemplate couchbaseTemplate, PlaceRepository placeRepository) {
+        this.cacheManager = cacheManager;
+        this.facebook = facebook;
+        this.couchbaseTemplate = couchbaseTemplate;
+        this.placeRepository = placeRepository;
+    }
+
+    @Scheduled(fixedDelay = 4000)
+    void janitor() {
+
+        Date date = new Date(System.currentTimeMillis() - (5 * 1000  ));
+        Query query = new Query();
+        query.setStale(Stale.FALSE);
+        query.setRangeEnd(Long.toString(date.getTime()));
+
+        placeRepository.findByInsertionDate(query).forEach(System.out::println);
+        System.out.println("hello, world! ");
+    }
+
+    @Cacheable(value = "places", key = "#query")
+    public List<String> search(String query, double lat, double lon, int distance) {
+        return facebook.placesOperations()
+                .search(query, lat, lon, distance)
+                .stream()
+                .map(p -> {
+                    Place objectToInsert = new Place(p);
+                    couchbaseTemplate.insert(objectToInsert);
+                    return objectToInsert;
+                })
+                .map(Place::getId)
+                .collect(Collectors.toList());
+    }
 }
 
 interface PlaceRepository extends CrudRepository<Place, String> {
 
     Collection<Place> findByFirstName(Query query);
+
+    Collection<Place> findByInsertionDate(Query query);
 }
 
 
@@ -142,9 +195,39 @@ class Place {
     @Field
     private String name, affilitation, category, description, about;
 
+    @Field
+    private Date insertionDate;
+
     @Id
     private String id;
 
+    public String getName() {
+        return name;
+    }
+
+    public String getAffilitation() {
+        return affilitation;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getAbout() {
+        return about;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Date getInsertionDate() {
+        return insertionDate;
+    }
 
     Place(Page p) {
         this.affilitation = p.getAffiliation();
@@ -153,9 +236,12 @@ class Place {
         this.category = p.getCategory();
         this.description = p.getDescription();
         this.about = p.getAbout();
-
+        this.insertionDate = new Date();
         org.springframework.social.facebook.api.Location pageLocation = p.getLocation();
         this.location = new Location(pageLocation);
+    }
+
+    Place() {
     }
 
     public Location getLocation() {
@@ -164,6 +250,9 @@ class Place {
 }
 
 class Location {
+    Location() {
+    }
+
     private String city;
     private String country;
     private String description;
